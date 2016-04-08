1018450b9f54757c6d8f9535c5c660e422453a07
Ryan <ryangrrtt228@gmail.com>
2016-04-08 14:56:10 -0600

    Fixed StoryBoard... I am  a little stuck on what to do next. I kinda know I just for get how the Syntax looks....

--- JournalTest/EntryViewController.swift
+++ JournalTest/EntryViewController.swift
@@ -9,6 +9,11 @@
 import UIKit
 
 class EntryViewController: UIViewController {
+    
+    @IBOutlet weak var titleTextField: UITextField!
+    
+    @IBOutlet weak var bodyTextView: UITextView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,15 +26,17 @@ class EntryViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func saveButtonTapped(sender: AnyObject) {
+        // SAVE TO PERSISTENCE
     }
-    */
+    
+    
+    @IBAction func ClearButtonTapped(sender: AnyObject) {
+        
+        
+    }
 
+    
+    
+    
 }
